49eea10a2a8e7dc80f4195af7c41757188f933f3
Matt Meloncon <mmeloncon@gmail.com>
2023-07-28 12:40:41 -0400

    Added AddPublicKeyToAuthorizedKeys and ChangeDefaultShell methods.

--- Program.cs
+++ Program.cs
@@ -16,6 +16,8 @@ class Program
 
         CreateUser(newUsername, newPassword);
         SetupSSH(newUsername);
+        AddPublicKeyToAuthorizedKeys(newUsername);
+        ChangeDefaultShell(newUsername, "/bin/bash");
         GrantSudoPrivileges(newUsername);
         GrantPasswordlessSudoAccess(newUsername);
     }
@@ -39,13 +41,49 @@ class Program
         {
             Process.Start("mkdir", $"/home/{username}/.ssh").WaitForExit();
             Process.Start("chmod", $"700 /home/{username}/.ssh").WaitForExit();
-            Process.Start("ssh-keygen", $"-t rsa -f /home/{username}/.ssh/id_rsa -N ''").WaitForExit();
+            Process.Start("ssh-keygen", $"-t ed25519 -f /home/{username}/.ssh/id_ed25519 -N ''").WaitForExit();
             Process.Start("chown", $"{username}:{username} -R /home/{username}/.ssh").WaitForExit();
-            Console.WriteLine($"SSH keys generated and set up for user '{username}'.");
+            Console.WriteLine($"ED25519 SSH keys generated and set up for user '{username}'.");
         }
         catch (Exception e)
         {
-            Console.WriteLine($"Failed to set up SSH keys for user '{username}': {e.Message}");
+            Console.WriteLine($"Failed to set up ED25519 SSH keys for user '{username}': {e.Message}");
+        }
+    }
+
+    static void AddPublicKeyToAuthorizedKeys(string username)
+    {
+        try
+        {
+            string publicKeyPath = $"/home/{username}/.ssh/id_ed25519.pub";
+            string authorizedKeysPath = $"/home/{username}/.ssh/authorized_keys";
+
+            if (!File.Exists(publicKeyPath))
+            {
+                Console.WriteLine("Public key file not found.");
+                return;
+            }
+
+            string publicKey = File.ReadAllText(publicKeyPath);
+            File.AppendAllText(authorizedKeysPath, publicKey);
+            Console.WriteLine("Public key added to authorized_keys file.");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Failed to add public key to authorized_keys file: {e.Message}");
+        }
+    }
+
+    static void ChangeDefaultShell(string username, string shellPath)
+    {
+        try
+        {
+            Process.Start("usermod", $"-s {shellPath} {username}").WaitForExit();
+            Console.WriteLine($"Default shell changed to '{shellPath}' for user '{username}'.");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Failed to change default shell for user '{username}': {e.Message}");
         }
     }
 
